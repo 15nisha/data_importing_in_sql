ae0d413e51c251aa91f46e0f8224f31302b0de16
15nisha <nishamadhav1998@gmail.com>
2021-12-02 15:28:04 +0530

    creating table

---
+++ checking_left_job.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host = "localhost", user= "root", password = "Nisha@15224", database = "testing")
+
+mycursor = mydb.cursor()
+
+query = "SELECT DISTINCT job_roles FROM table3 where ID = 10;"
+mycursor.execute(query)
+list1 =[]
+myresult = mycursor.fetchall()
+for row in myresult:
+    x = row[0]
+    list1.append(x)
+
+query = "SELECT job_roles FROM table2 where Id = 10;"
+mycursor.execute(query)
+result = []
+list2 =[]
+myresult = mycursor.fetchall()
+for row in myresult:
+    y = row[0]
+    list2.append(y)
+for i in list2:
+    if i not in set(list1):
+        result.append(i)
+print(result)
